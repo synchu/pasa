b827fcaa4832563344c747d0703b81338d8de352
nnyagolov <synchu@synchu.com>
2016-01-13 18:03:47 +0200

    Add task adding features

---
+++ src/assets/javascripts/navbar.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppBar from 'react-toolbox/lib/app_bar';
+import Button from 'react-toolbox/lib/button';
+
+const MainAppBar = () => (
+    <AppBar className={style.appbar} flat>
+        <h1 className={style.title}>React Toolbox Example</h1>
+        <Button className={style.button} icon="add" floating accent/>
+    </AppBar>
+);
+
+export default MainAppBar;
+
+class NavBar extends React {
+    render() {
+        return (
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"><img class="brandimage img-responsive" alt="Brand"
+                                                              src="images/logo.jpg"></a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li><a href="products.html">Products<span class="sr-only">(current)</span></a></li>
+                            <li class="active"><a href="index.html">Tasks</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-haspopup="true"
+                                   aria-expanded="false">Actions<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">New resource</a></li>
+                                    <li><a href="#">New calendar</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">New task completion time</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">Reset</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="navbar-form navbar-left" role="search">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                    </div>
+                    <!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container-fluid -->
+            </nav>
+    );
+    }
+    }
